ce5568406e075a9057407d056409fcbb80829324
HSSINE Younes <hssine.younes@emsi-edu.ma>
2024-02-19 11:31:10 +0100

    Post model and HttpService added

--- postsPrj/src/app/app.component.ts
+++ postsPrj/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from './models/Post';
+import { HttpService } from './services/http.service';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'postsPrj';
+  constructor(private httpService: HttpService) { }
+  posts !: Post[];
+  updateForme !: FormGroup;
+  Updated!: Post;
+  isFormVisible: boolean = false;
+
+  ngOnInit(): void {
+    this.httpService.getAll().subscribe(post => this.posts = post);
+  }
 }
